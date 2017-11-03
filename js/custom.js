9adc907f4ec5545e51b9011556cf7d8d1e1a919a
unknown <sangani.reeta@gmail.com>
2017-11-03 19:07:21 +0530

    initial commit.

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+
+jQuery(document).ready(function () {
+	
+	 jQuery('.send').click(function(){
+		  var validflag = true;
+		//Name 
+				
+				if(jQuery('.input-sm').val().trim()=="" ){
+					$('.input-sm').css({'border':'1px solid #ff0000'});
+					$("#error").show();
+                              alert('Plz Enter Zipcode');
+					validflag = false;
+				}
+				else{
+					$('.input-sm').css({'color':'#333333'});
+					$("#error").hide();
+				}
+				
+		
+		
+	});
+			 
+			 // Focus and blure 
+				jQuery('.input-sm').focus(function(){
+						$(this).css({'border-color':'#000000'});
+						$("#error").hide();
+                              
+				});
+				
+				jQuery('.input-sm').blur(function(){
+					
+						$(this).css({'border-color':'#e2e2e2'});
+
+				});
+				
+			
+			 
+				
+						
+});
+
