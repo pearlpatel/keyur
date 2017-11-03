073d70beccbbf03e27ce99d4301b1f8af59daa38
unknown <sangani.reeta@gmail.com>
2017-11-03 19:04:48 +0530

    initial commit.

---
+++ config/config.php
@@ -0,0 +1,101 @@
+<?php
+
+session_save_path("/tmp");
+
+@session_start();
+
+date_default_timezone_set("Asia/Kolkata");
+
+// Site Setting
+
+define('SITE_NAME', 'Admin For App');
+
+define('SITE_PHONE', '');
+
+// Global Setting
+define('BASE_PATH', dirname(dirname(__FILE__)));
+
+define('BASE_URL', 'http://rapidllc.online/appAdmin/');
+
+define('INDEX', 'index.php');
+
+define('DS', DIRECTORY_SEPARATOR);
+
+define('APP_URL', BASE_URL.'app/');
+
+define('API_URL', BASE_URL.INDEX.'/'.'api/');
+
+define('ADMIN_URL', BASE_URL.INDEX.'/'.'admin/');
+
+define('APP_JS', BASE_URL.'app/views/cms/js/');
+
+define('USER_APP_JS', BASE_URL.'app/views/cms/js/');
+
+define('URL', BASE_URL.INDEX.'/');
+
+ini_set('session.save_path',BASE_PATH.DS.'tmp'.DS.'session');
+
+error_reporting(0);
+
+define('DEBUG_MODE', true);
+
+define('DB_NAME', 'djwor5ey_appadmin');
+
+define('DB_USER', 'djwor5ey');
+
+define('DB_PASSWORD', '$PRELAX$cozer99');
+
+define('DB_HOST', 'localhost');
+
+define('DB_CHARSET', 'utf8');
+
+define('DB_COLLATE', '');
+
+/*****	Dir Settings	*****/
+
+define('DIR_CONFIG',BASE_PATH.DS.'config');
+
+define('DIR_APP',BASE_PATH.DS.'app');
+
+define('DIR_SYS',BASE_PATH.DS.'sys');
+
+define('DIR_CONTROLLER',DIR_APP.DS.'controllers');
+
+define('DIR_MODAL',DIR_APP.DS.'modals');
+
+define('DIR_VIEW',DIR_APP.DS.'views');
+
+define('DIR_LIBRARY',BASE_PATH.DS.'lib');
+
+define('UPLOAD_DIR','uploads');
+
+define('UPLOAD_LOGO','uploads/logo/');
+define('UPLOAD_ICON','uploads/icon/');
+define('UPLOAD_BANNER','uploads/promoBanners/');
+
+define('DEFAULT_CONTROLLER','index');
+
+define('DEFAULT_ACTION','index');
+
+// Load Class	//
+require_once(DIR_LIBRARY.'/classes/class.database-access.php');
+
+require_once(DIR_LIBRARY.'/classes/class.url.php');
+
+require_once(DIR_LIBRARY.'/classes/class.input.php');
+
+require_once(DIR_LIBRARY.'/classes/class.file.php');
+
+//require_once(DIR_SYS.'/core/template.php');
+
+require_once(DIR_SYS.'/core/controller.php');
+
+require_once(DIR_SYS.'/core/modal.php');
+
+require_once(BASE_PATH.'/lib/helpers/helper.debug.php');
+
+$Url = new Url(BASE_URL.INDEX);
+
+$ApiUrl = new Url(API_URL);
+
+$AdminUrl = new Url(ADMIN_URL);
---
+++ config/route.php
@@ -0,0 +1,13 @@
+<?php
+$route = array();
+$admin_route = array('');
+$admin_route[''] = 'admin/login';
+$admin_route['login'] = 'admin/login';
+$admin_route['developer'] = 'admin/login';
+$admin_route['app'] = 'app/apps';
+$admin_route['link'] = 'link/link';
+$admin_route['hotLink'] = 'hotLink/link';
+$admin_route['devLink'] = 'devLink/link';
+$admin_route['viewappdetail'] = 'app/viewAppDetail';
+$admin_route['developer'] = 'admin/developer';
+$admin_route['logout'] = 'admin/logout';
